38ec3cd5bd25377fae9fe54c773f19d25cbeda42
Melchas <melanyvp98@gmail.com>
2023-06-06 16:47:00 -0600

    quitando espacio de mas en css

--- versionMel/Encriptador.js
+++ versionMel/Encriptador.js
@@ -1,9 +1,28 @@
+//variables donde almacenamos la informacion de los espacios de texto o los textareas existentes
+const textArea = document.querySelector(".input");
+const resultado = document.querySelector(".output");
 
+//almacenar llaves de encriptacion dentro de una matriz
+let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
-const btnCopiar = document.querySelector(".copiar");
-btnCopiar.addEventListener("click", copiar = () => {
-var contenido = document.querySelector(".texto-resultado").textContent;
-navigator.clipboard.writeText(contenido);
-console.log("hola");
-}) 
+//evento para que al click en boton encriptar la funcion encriptar ocurra
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  resultado.value = textoEncriptado;
+}
 
+//funcion para encriptar el texto input
+
+function encriptar(stringEncriptado) {
+  stringEncriptado = stringEncriptado.toLowerCase();
+  
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptado.includes(matrizCodigo[i][0])) {
+      stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
+  
+  return stringEncriptado;
+}
+
+console.log(matrizCodigo);
