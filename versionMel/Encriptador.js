a13a377159b6b2e78a1f114ed7f19fab41db9055
Melchas <melanyvp98@gmail.com>
2023-06-06 15:56:20 -0600

    conectando archivo html con js

--- versionMel/Encriptador.js
+++ versionMel/Encriptador.js
@@ -1,6 +1,9 @@
+
+
 const btnCopiar = document.querySelector(".copiar");
 btnCopiar.addEventListener("click", copiar = () => {
 var contenido = document.querySelector(".texto-resultado").textContent;
 navigator.clipboard.writeText(contenido);
 console.log("hola");
 }) 
+
