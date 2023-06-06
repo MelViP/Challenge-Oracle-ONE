7d7e8ae57678902b498d2063602414bff35c6899
Melchas <melanyvp98@gmail.com>
2023-06-06 00:32:47 -0600

    nuevo archivo JS para version mel y agregando responsividad al site

---
+++ versionMel/Encriptador.js
@@ -0,0 +1,6 @@
+const btnCopiar = document.querySelector(".copiar");
+btnCopiar.addEventListener("click", copiar = () => {
+var contenido = document.querySelector(".texto-resultado").textContent;
+navigator.clipboard.writeText(contenido);
+console.log("hola");
+}) 
