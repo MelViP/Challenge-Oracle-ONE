a9eb3ab443fd8cb0a49f60048be21115723caae1
Melchas <melanyvp98@gmail.com>
2023-06-06 19:37:22 -0600

    proyecto final

--- versionMel/Encriptador.js
+++ versionMel/Encriptador.js
@@ -8,95 +8,95 @@ const resultado = document.querySelector(".output");
 
 
 //almacenar llaves de encriptacion dentro de una matriz
-let llavesEncriptado = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+var llavesEncriptado = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
 
 //evento para que al click en boton encriptar la funcion encriptar ocurra
 function btnEncriptar() {
-  const textoEncriptado = encriptar(textArea.value);
-  resultado.value = textoEncriptado;
+    const textoEncriptado = encriptar(textArea.value);
+    resultado.value = textoEncriptado;
 
-  textArea.value = ""; //limpiar campo cuando aparece resultado desencriptado
-  resultado.style.backgroundImage = "none";
-  getElementbyClass(descripcionResultado).shift 
+    textArea.value = ""; //limpiar campo cuando aparece resultado desencriptado
+    resultado.style.backgroundImage = "none";
 
+    // Ocultar el texto de descripción
+    const textoResultado = document.getElementById("textoResultado");
+    textoResultado.classList.add("oculto");
 
+    //hacer que boton copiar apareza
+    const copiarBoton = document.getElementById("copiar");
+    copiarBoton.classList.remove("oculto");
 }
 
 //funcion para encriptar el texto input
 
 function encriptar(stringEncriptado) {
-  stringEncriptado = stringEncriptado.toLowerCase();
-  
-  for (let i = 0; i < llavesEncriptado.length; i++) {
-    if (stringEncriptado.includes(llavesEncriptado[i][0])) {
-      stringEncriptado = stringEncriptado.replaceAll(llavesEncriptado[i][0], llavesEncriptado[i][1]);
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < llavesEncriptado.length; i++){
+        if(stringEncriptado.includes(llavesEncriptado[i][0])){
+           stringEncriptado = stringEncriptado.replaceAll(llavesEncriptado[i][0], llavesEncriptado[i][1]);
+        }
     }
-  }
-  
-  return stringEncriptado;
+    return stringEncriptado;
 }
+
 
 
 
 // DESENCRIPTACION
 
 //almacenar llaves de encriptacion dentro de una matriz
-let llavesDesencriptado = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+var llavesDesencriptado = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
 
 
 //evento para que al click en boton encriptar la funcion encriptar ocurra
+
 function btnDesencriptar() {
-  const textoDesencriptado = desencriptar(textArea.value);
-  resultado.value = textoDesencriptado;
+    const textoDesencriptado = desencriptar(textArea.value);
+    resultado.value = textoDesencriptado;
+  
+    textArea.value = ""; //limpiar campo cuando aparece resultado encriptado
+    resultado.style.backgroundImage = "none";
 
-  textArea.value = ""; //limpiar campo cuando aparece resultado encriptado
-  resultado.style.backgroundImage = "none";
+    // Ocultar el texto de descripción
+    const textoResultado = document.getElementById("textoResultado");
+    textoResultado.classList.add("oculto");
 
+    //hacer que boton copiar apareza
+    const copiarBoton = document.getElementById("copiar");
+    copiarBoton.classList.remove("oculto");
+    
 }
 
 //funcion para encriptar el texto input
 
 function desencriptar(stringDesencriptado) {
-  stringDesencriptado = stringDesencriptado.toLowerCase();
+    //hacer minusculas
+    stringDesencriptado = stringDesencriptado.toLowerCase();
   
-  for (let i = 0; i < llavesDesencriptado.length; i++) {
-    if (stringDesencriptado.includes(llavesDesencriptado[i][0])) {
-      stringDesencriptado = stringDesencriptado.replaceAll(llavesDesencriptado[i][0], llavesDesencriptado[i][1]);
+    for (let i = 0; i < llavesDesencriptado.length; i++) {
+
+        if (stringDesencriptado.includes(llavesDesencriptado[i][0])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(llavesDesencriptado[i][0], llavesDesencriptado[i][1]);
+        }
     }
-  }
   
   return stringDesencriptado;
 }
 
-//COPIAR AL PORTAPAPELES
+//copiar al portapepeles boton
 
-//llamar variable del boton copiar
+const copiarBoton = document.getElementById("copiar");
+copiarBoton.addEventListener("click", copiarAlPortapapeles);
 
-const botonCopiar = document.querySelector('.copiar');
+function copiarAlPortapapeles() {
+    const resultado = document.querySelector(".output");
+    resultado.select();
+    document.execCommand("copy");
 
-//llamar el textarea del texto resultado
-
-const textarea = document.querySelector('.output');
-
-
-function portapapeles() {
-    try {
-      // Seleccionar el contenido del textarea
-      const textarea = document.getElementById('myTextarea');
-      textarea.select();
-  
-      // Copiar el texto al portapapeles
-      const exito = document.execCommand('copy');
-  
-      // Verificar si la copia fue exitosa
-      if (exito) {
-        console.log('Texto copiado al portapapeles');
-      } else {
-        console.error('Error al copiar al portapapeles');
-      }
-    } catch (error) {
-      console.error('Error al copiar al portapapeles:', error);
-    }
-  }
+    // Cambiar el texto del botón a "Copiado"
+    const copiarBoton = document.getElementById("copiar");
+    copiarBoton.textContent = "Copiado";
+}
   
